593b1d8e4f6fc738af161da51750284724bf233c
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-09-23 16:48:31 -0300

    Database first feito com sucesso.

---
+++ Domains/Curso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EduX_API.Domains
+{
+    public partial class Curso
+    {
+        public int IdCurso { get; set; }
+        public string Titulo { get; set; }
+        public int? IdInstituicao { get; set; }
+
+        public virtual Instituicao IdInstituicaoNavigation { get; set; }
+    }
+}
