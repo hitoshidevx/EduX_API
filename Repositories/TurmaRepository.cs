eff73d0bfd1b1d8fdba2b03fbb27e7d5f187944a
Tiago Valim <tiago.valim@gmail.com>
2020-09-24 20:27:08 -0300

    att

---
+++ Repositories/TurmaRepository.cs
@@ -0,0 +1,37 @@
+﻿using EduX_API.Domains;
+using EduX_API.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduX_API.Repositories
+{
+    public class TurmaRepository : ITurma
+    {
+        public void Adicionar(Turma t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(Guid id, Turma t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Turma BuscarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Turma> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
